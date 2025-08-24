4b0944408cfeef7b55982e2a0f14db54a1d88e3a
FAL <falworks.contact@gmail.com>
2025-08-24 18:53:09 +0900

    chore: add assertExtends utility function

---
+++ src/core/type-utils.ts
@@ -0,0 +1,6 @@
+/**
+ * Asserts that the second type extends the first type.
+ * @example assertExtends<Base, Derived>();
+ * @internal
+ */
+export declare function assertExtends<Super, Sub extends Super>(_?: Sub): void;
