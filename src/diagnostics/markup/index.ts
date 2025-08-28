db4c225d3ab33f33d09de91903afc3d227e95f31
FAL <falworks.contact@gmail.com>
2025-08-28 17:58:41 +0900

    refactor: explicit exports from markup directory

--- src/diagnostics/markup/index.ts
+++ src/diagnostics/markup/index.ts
@@ -2,5 +2,26 @@
  * Components for working with structured markup representations.
  */
 
-export * from "./builders.ts";
-export * from "./types.ts";
+// Factory functions
+export {
+	bulletList,
+	code,
+	example,
+	exampleInput,
+	exampleList,
+	exampleOutput,
+	hint,
+	introducer,
+	listItem,
+	markup,
+	orderedList,
+	paragraph,
+	setCaption,
+	stepwiseInstructionList,
+	strong,
+	task,
+	text,
+} from "./builders.ts";
+
+// Types and interfaces
+export type * from "./types.ts";
